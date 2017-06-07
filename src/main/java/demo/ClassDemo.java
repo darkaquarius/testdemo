6524dcf9f560e5b742bc33e294cad538cb137f45
huishen <darkaquarius88@163.com>
2017-06-07 15:27:47 +0800

    class

---
+++ src/main/java/demo/ClassDemo.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.junit.Test;
+
+/**
+ * Created by huishen on 17/6/6.
+ *
+ */
+public class ClassDemo {
+
+    @Test
+    public void test() {
+        String name = String.class.getName();
+        System.out.println(name);
+    }
+
+}
--- src/main/java/demo/ClassLoaderDemo.java
+++ src/main/java/demo/ClassLoaderDemo.java
@@ -1,11 +1,18 @@
 package demo;
 
+import org.junit.Test;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import sun.misc.Launcher;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Enumeration;
+import java.util.Properties;
 
 /**
  * Created by huishen on 17/5/10.
+ *
  */
 public class ClassLoaderDemo {
 
@@ -14,6 +21,29 @@ public class ClassLoaderDemo {
         for (int i = 0; i < urLs.length; i++) {
             System.out.println(urLs[i].toExternalForm());
         }
+    }
+
+    @Test
+    public void test() {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Enumeration<URL> urls = null;
+        try {
+            urls = classLoader != null ? classLoader.getResources("META-INF/spring.factories") :
+                ClassLoader.getSystemResources("META-INF/spring.factories");
+
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                Properties properties = PropertiesLoaderUtils.loadProperties(new UrlResource(url));
+                // String factoryClassNames = properties.getProperty(factoryClassName);
+                // result.addAll(Arrays.asList(StringUtils.commaDelimitedListToStringArray(factoryClassNames)));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 
 }
