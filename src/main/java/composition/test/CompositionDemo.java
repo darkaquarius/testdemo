fbfa4e7564278a1308468bdd92a624d3ad858e71
huishen <darkaquarius88@163.com>
2017-06-12 20:50:38 +0800

    CompositionDemo.test

---
+++ src/main/java/composition/test/CompositionDemo.java
@@ -0,0 +1,22 @@
+package composition.test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * Created by huishen on 17/6/12.
+ *
+ */
+public class CompositionDemo {
+
+    public static void main(String[] args) {
+        InstrumentedSet<String> s =
+            new InstrumentedSet<>(new HashSet<>());
+
+        s.addAll(Arrays.asList("zhangsan", "lisi", "wangwu"));
+
+        int addCount = s.getAddCount();
+        System.out.println("aaddCount: " + addCount);
+    }
+
+}
---
+++ src/main/java/composition/test/InstrumentedSet.java
@@ -0,0 +1,92 @@
+package composition.test;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by huishen on 17/6/12.
+ *
+ */
+public class InstrumentedSet<E> implements Set<E> {
+
+    private int addCount = 0;
+
+    private final Set<E> s;
+
+    public InstrumentedSet(Set<E> s) {
+        this.s = s;
+    }
+
+    @Override
+    public int size() {
+        return s.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return s.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return s.contains(o);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return s.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return s.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return s.toArray(a);
+    }
+
+    @Override
+    public boolean add(E e) {
+        addCount++;
+        return s.add(e);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return s.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return s.containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return s.addAll(c);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return s.retainAll(c);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return s.removeAll(c);
+    }
+
+    @Override
+    public void clear() {
+        s.clear();
+    }
+
+    public int getAddCount() {
+        return addCount;
+    }
+
+}
