6cc5862718a3b0968f1858e9ff29e9b6e6147fc3
huishen <darkaquarius88@163.com>
2017-10-09 18:13:34 +0800

    EntityUtils.consume(entity);

--- src/main/java/httpClientdemo/HttpClientUtil.java
+++ src/main/java/httpClientdemo/HttpClientUtil.java
@@ -67,7 +67,15 @@ public class HttpClientUtil {
                 // httpClient.close();
                 if (response != null) {
                     response.close();
-                    EntityUtils.consume(entity);
+                    // consume()其实就是instream.close()，作用就是将用完的连接释放，下次请求可以复用。
+                    // 如果不使用in.close();而仅仅使用response.close();结果就是连接会被关闭，并且不能被复用，这样就失去了采用连接池的意义。
+                    // EntityUtils.consume(entity);
+
+                    // 连接池释放连接的时候，并不会直接对TCP连接的状态有任何改变，
+                    // 只是维护了两个Set，leased和avaliabled，leased代表被占用的连接集合，avaliabled代表可用的连接的集合，
+                    // 释放连接的时候仅仅是将连接从leased中remove掉了，并把连接放到avaliabled集合中
+                    // without throwing any IOException.
+                    EntityUtils.consumeQuietly(entity);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
