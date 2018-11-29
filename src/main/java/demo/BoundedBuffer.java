e44d371acc4da332921d4501b9a5e656ea9e5648
huishen <darkaquarius88@163.com>
2018-11-29 11:12:06 +0800

    模拟ArrayBlockingQueue

---
+++ src/main/java/demo/BoundedBuffer.java
@@ -0,0 +1,66 @@
+package demo;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author huishen
+ * @date 2018/11/28 下午5:59
+ * <p>
+ * ArrayBlockongQueue采用了这种方式实现了生产者-消费者，实际生产中可以直接使用ArrayBlockingQueue
+ */
+public class BoundedBuffer {
+
+    private final Lock lock = new ReentrantLock();
+
+    // condition 依赖于 lock 来产生
+    private final Condition notFull = lock.newCondition();
+
+    private final Condition notEmpty = lock.newCondition();
+
+    private final Object[] items = new Object[100];
+
+    private int putptr, takeptr, count;
+
+    /**
+     * 生产
+     */
+    public void put(Object x) throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == items.length) {
+                notFull.await();  // 队列已满，等待，直到 not full 才能继续生产
+            }
+            items[putptr] = x;
+            if (++putptr == items.length) {
+                putptr = 0;
+            }
+            ++count;
+            notEmpty.signal(); // 生产成功，队列已经 not empty 了，发个通知出去
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * 消费
+     */
+    public Object take() throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == 0) {
+                notEmpty.await(); // 队列为空，等待，直到队列 not empty，才能继续消费
+            }
+            Object x = items[takeptr];
+            if (++takeptr == items.length) {
+                takeptr = 0;
+            }
+            --count;
+            notFull.signal(); // 被我消费掉一个，队列 not full 了，发个通知出去
+            return x;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
